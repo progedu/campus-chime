b6b17d6f9e5f59808f0799e4f2bd7d86aad639b5
rntkym <shimizu.ran.0618@gmail.com>
2021-02-03 10:33:58 +0900

    Initial commit

---
+++ chime.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+echo "mpg321 /home/pi/chime/chime.mp3" | at 9:30
+echo "mpg321 /home/pi/chime/chime.mp3" | at 9:45
+echo "mpg321 /home/pi/chime/chime.mp3" | at 10:35
+echo "mpg321 /home/pi/chime/chime.mp3" | at 10:45
+echo "mpg321 /home/pi/chime/chime.mp3" | at 11:35
+echo "mpg321 /home/pi/chime/chime.mp3" | at 11:45
+echo "mpg321 /home/pi/chime/chime.mp3" | at 12:35
+echo "mpg321 /home/pi/chime/chime.mp3" | at 13:15
+echo "mpg321 /home/pi/chime/chime.mp3" | at 14:05
+echo "mpg321 /home/pi/chime/chime.mp3" | at 14:15
+echo "mpg321 /home/pi/chime/chime.mp3" | at 15:05
+echo "mpg321 /home/pi/chime/chime.mp3" | at 15:15
+echo "mpg321 /home/pi/chime/chime.mp3" | at 16:05
+echo "mpg321 /home/pi/chime/chime.mp3" | at 16:15
+echo "mpg321 /home/pi/chime/chime.mp3" | at 17:30
